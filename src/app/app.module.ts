a8e4b46d6c02e00aa2ba2e0cbc82694d8151d828
Aleksander <>
2019-02-26 16:34:01 +0100

    First section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './content/header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './content/header/header.component';
+
+//  owl carousel import
+
+import {OwlModule} from 'ngx-owl-carousel';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        OwlModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/content/header/header.component.ts
+++ src/app/content/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    // mySlideImages = [1, 2, 3].map((i) => `https://picsum.photos/640/480?image=${i}`);
+    mySlideImages = ['../../assets/a1.png'];
+    myCarouselImages = [1, 2, 3, 4, 5, 6].map((i) => `https://picsum.photos/640/480?image=${i}`);
+    mySlideOptions = {items: 1, dots: true, nav: true};
+    myCarouselOptions = {items: 3, dots: true, nav: true};
 
-  ngOnInit() {
-  }
+    constructor() {
+        console.log(this.mySlideImages);
+        console.log(this.myCarouselImages);
+        console.log(this.mySlideOptions);
+        console.log(this.myCarouselOptions);
+    }
+
+    ngOnInit() {
+    }
 
 }
