637e991177bbb7eb2eb502e56d5ee0294eca6089
Aleksander <>
2019-02-27 20:14:28 +0100

    Product desktop done.

--- src/app/content/products/products.component.ts
+++ src/app/content/products/products.component.ts
@@ -5,7 +5,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
-  animations: [
+/*  animations: [
     trigger('EnterLeave', [
       state('flyIn', style({ transform: 'translateY(0)' })),
       transition(':enter', [
@@ -16,22 +16,22 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         animate('0.5s ease-out', style({ transform: 'translateY(-100px)', opacity: 1 }))
       ])
     ])
-  ]
+  ]*/
 })
 export class ProductsComponent implements OnInit {
   products = [
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
-    {url: '../../../assets/a1.png', text: 'lorem ipsum lorem ipsum'},
+    {url: '../../../assets/a1.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/a1w.png'},
+    {url: '../../../assets/a2.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/a2w.png'},
+    {url: '../../../assets/a3.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/a3w.png'},
+    {url: '../../../assets/a4.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/a4w.png'},
+    {url: '../../../assets/b1.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/b1w.png'},
+    {url: '../../../assets/b2.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/b2w.png'},
+    {url: '../../../assets/b3.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/b3w.png'},
+    {url: '../../../assets/b4.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/b4w.png'},
+    {url: '../../../assets/c1.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/c1w.png'},
+    {url: '../../../assets/c2.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/c2w.png'},
+    {url: '../../../assets/c3.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/c3w.png'},
+    {url: '../../../assets/c4.png', hoverText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco', urlOnHover: '../../../assets/c4w.png'},
   ];
 
   isHiddenElement = false;
@@ -39,9 +39,9 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
   }
-  showHiddenText(){
+/*  showHiddenText(){
     console.log(this.isHiddenElement);
     this.isHiddenElement = !this.isHiddenElement;
-  }
+  }*/
 
 }
